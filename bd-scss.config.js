b35a623b1ce672cdf95a52095aa99508e0f9a5db
Ian <Gibbu_@hotmail.com>
2022-10-27 11:15:33 +1100

    Fixed for update

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -4,7 +4,8 @@ export default {
 		name: 'RadialStatus',
 		author: 'Gibbu#1211',
 		version: '2.0.0',
-		description: 'Changes the status icons to wrap around the avatar. Shape can be changed inside the theme file.',
+		description:
+			'Changes the status icons to wrap around the avatar. Shape can be changed inside the theme file.',
 		source: 'https://github.com/DiscordStyles/RadialStatus',
 		invite: 'ZHthyCw',
 		website: 'https://www.gibbu.me'
@@ -13,4 +14,4 @@ export default {
 		output: 'dist/dist'
 	},
 	baseImport: 'https://discordstyles.github.io/RadialStatus/dist/RadialStatus.css'
-}
+};
