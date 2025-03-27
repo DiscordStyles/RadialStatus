edaa74ddc0b99fcadbc1ff89e5a642502ff23e79
Gibbu <Gibbu_@hotmail.com>
2025-03-27 11:34:35 +1100

    Discord update

--- bd-scss.config.js
+++ bd-scss.config.js
@@ -7,11 +7,7 @@ export default {
 		description:
 			'Changes the status icons to wrap around the avatar. Shape can be changed inside the theme file.',
 		source: 'https://github.com/DiscordStyles/RadialStatus',
-		invite: 'ZHthyCw',
-		website: 'https://www.gibbu.me'
+		invite: 'ZHthyCw'
 	},
-	base: {
-		output: 'dist/dist'
-	},
-	baseImport: 'https://discordstyles.github.io/RadialStatus/dist/RadialStatus.css'
+	github: 'DiscordStyles'
 };
